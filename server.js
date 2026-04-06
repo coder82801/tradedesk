440997b4a25306e39f0fa766e739ef6529529dc8
coder82801 <2200004376@stu.iku.edu.tr>
2026-04-06 22:26:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const ALPACA_API_KEY = process.env.ALPACA_API_KEY || "";
 const ALPACA_SECRET_KEY = process.env.ALPACA_SECRET_KEY || "";
 const ALPACA_BASE_URL =
